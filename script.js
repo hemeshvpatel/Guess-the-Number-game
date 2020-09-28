5bc4f1c7a7cb19221e064761248e41a8ef1ab224
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-09-27 20:22:55 -0500

    Console whether the guess is too high, too low, or is correct inside playGame function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * Guess The Number Game
  * DONE: Get user value from input and save it to variable numberGuess
- * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
+ * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
  * TODO: Console whether the guess is too high, too low, or is correct inside playGame function
  * TODO: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
@@ -28,7 +28,13 @@ window.onload = function () {
 function playGame() {
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById("number-guess").value;
-  console.log(numberGuess);
+  if (numberGuess > correctNumber) {
+    console.log("too high");
+  } else if (numberGuess < correctNumber) {
+    console.log("too low");
+  } else {
+    console.log("is correct");
+  }
 }
 
 /**

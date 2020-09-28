88c7bf99ecfd1040bc4f482ba83e64b024415ff7
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-09-27 20:19:35 -0500

    Generate a random number 1 to 100 and save it to variable correctNumber

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * Guess The Number Game
- * TODO: Get user value from input and save it to variable numberGuess
+ * DONE: Get user value from input and save it to variable numberGuess
  * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
  * TODO: Console whether the guess is too high, too low, or is correct inside playGame function
  * TODO: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
@@ -14,6 +14,8 @@
 // Variable to store the list of guesses
 
 // Variable for store the correct random number
+let correctNumber = getRandomNumber();
+console.log(correctNumber);
 
 window.onload = function () {
   document.getElementById("number-submit").addEventListener("click", playGame);
@@ -56,6 +58,9 @@ function resetResultContent() {
  */
 function getRandomNumber() {
   // *CODE GOES BELOW HERE *
+  let randomNumber = Math.random();
+  let wholeNumber = Math.floor(randomNumber * 100) + 1;
+  return wholeNumber;
 }
 
 /**
